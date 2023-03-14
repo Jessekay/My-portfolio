a5b4cd3b8991d39252a42b2282faae4b0eed8664
nilab-khwaja <nilab.khwajazada1212@gmail.com>
2023-03-14 23:01:56 +0430

    update js file

--- index.js
+++ index.js
@@ -1,22 +1,18 @@
-const hamburger = document.querySelector(".hamburger");
-const newE = document.querySelector(".new");
-const Menu = document.querySelector(".menu");
+const hamburger = document.querySelector('.hamburger');
+const navMenu = document.querySelector('.menu');
+const body = document.querySelector('body');
+function mobileMenu() {
+  hamburger.classList.toggle('active');
+  navMenu.classList.toggle('active');
+  body.classList.toggle('scroll-lock');
+}
+hamburger.addEventListener('click', mobileMenu);
 
-hamburger.addEventListener("click", () => {
-    console.log('clicked')
-    hamburger.classList.toggle("active");
-    Menu.classList.toggle("active");
-})
+const navLink = document.querySelectorAll('.nav-link');
 
-newE.addEventListener("click", () => {
-    console.log('clicked')
-    newE.classList.toggle("active");
-    Menu.classList.toggle("active");
-})
-
-
-document.querySelectorAll(".nav-link").forEach(N => n.addEventListener("click", () => {
-    hamburger.classList.remove("active");
-    newE.classList.remove("active");
-    Menu.classList.remove("active");
-}))
+function closeMenu() {
+  hamburger.classList.remove('active');
+  navMenu.classList.remove('active');
+  body.classList.remove('scroll-lock');
+}
+navLink.forEach((n) => n.addEventListener('click', closeMenu));
