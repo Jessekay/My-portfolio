5147a16faa375b5e2e331f107f0099ee8347f845
Jesse <kayjes00@gmail.com>
2023-03-17 12:38:20 +0200

    stored data in local-storage

--- index.js
+++ index.js
@@ -278,3 +278,27 @@ form.addEventListener('submit', (e) => {
     error.innerHTML = 'invalid email!';
   }
 });
+
+//preserved data
+
+const uName = document.getElementById('fullname');
+const uEmail = document.getElementById('email_address');
+const text = document.getElementById('text_message');
+const pForm = document.getElementById('form');
+
+pForm.addEventListener('change', () => {
+  const data = {
+    username: uName.value,
+    useremail: uEmail.value,
+    message: text.value,
+  };
+  const userDataSerialized = JSON.stringify(data);
+  localStorage.setItem('data', userDataSerialized);
+});
+
+window.addEventListener('load', () => {
+  const userDataSerData = JSON.parse(localStorage.data);
+  uName.value = userDataSerData.username;
+  uEmail.value = userDataSerData.useremail;
+  text.value = userDataSerData.message;
+});
